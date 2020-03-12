0dcc011b4249f333421d3738aea63523b5d6d898
xuweixin <xuweixin@ibingniao.com>
2020-03-12 11:08:51 +0800

    xu: add upload extend

--- app/app1/controller/Index.php
+++ app/app1/controller/Index.php
@@ -4,11 +4,13 @@ namespace app\app1\controller;
 use app\app2\commons\TestFacade;
 use app\app2\commons\TestFacadeInstance;
 use app\BaseController;
+use think\facade\Log;
 
 class Index extends BaseController
 {
     public function index()
     {
+        Log::write(1111111);
         session('test0', 1111);
         var_dump($this->request->session());
 //        TestFacade::haha();
